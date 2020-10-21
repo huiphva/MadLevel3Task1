997ab41e24a7162fe8807222e2acd49175f4f63d
huiphva <huip.van.den.ende@hva.nl>
2020-10-21 10:22:24 +0200

    Initial commit

---
+++ app/src/main/java/io/huip/madlevel3task1/RatingFragment.kt
@@ -0,0 +1,50 @@
+package io.huip.madlevel3task1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_rating.*
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class RatingFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_rating, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnRatingFragment.setOnClickListener {
+            navigateToSummary()
+        }
+
+        showRandomAssessableGame()
+
+    }
+
+    private fun showRandomAssessableGame() {
+        val randomGame = listOf("Red Dead Redemption 2", "Rocket League",
+            "Shadow of the Tombraider").random()
+
+        tvRatingSubTitle.text = randomGame
+    }
+
+    private fun navigateToSummary() {
+        val args = Bundle()
+        args.putFloat(ARG_GAME_RATING, rbRatingFragment.rating)
+        args.putString(ARG_GAME_NAME, tvRatingSubTitle.text.toString())
+
+        findNavController().navigate(R.id.action_RatingFragment_to_summaryFragment, args)
+    }
+}
---
+++ app/src/main/java/io/huip/madlevel3task1/SummaryFragment.kt
@@ -0,0 +1,41 @@
+package io.huip.madlevel3task1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_summary.*
+
+const val ARG_GAME_NAME = "arg_game_name"
+const val ARG_GAME_RATING = "arg_game_rating"
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class SummaryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_summary, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val gameName = arguments?.getString(ARG_GAME_NAME)
+        val gameRating = arguments?.getFloat(ARG_GAME_RATING)
+
+        tvSummary.text =
+            String.format("You rated %s with %.1f stars! Thanks?", gameName, gameRating)
+
+        btnRestart.setOnClickListener {
+            findNavController().navigate(R.id.action_summaryFragment_to_StartFragment)
+        }
+    }
+}
+
